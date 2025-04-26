741427098ec1a474645405082e469b3e75fd9c49
Nikhil-4102 <nikhilmalave04ofcl@gmail.com>
2025-04-26 19:26:05 +0530

    task no. , task , and delete btn added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'TODO-List';
+  task="";
+  taskList:{id:number, task:string}[]=[];
+
+  addTask(){
+    this.taskList.push({id:this.taskList.length+1, task:this.task});
+    this.task="";
+    console.log(this.taskList);
+  }
+
+  deleteTask(taskID:number){
+    this.taskList = this.taskList.filter((item) => item.id !== taskID);
+    console.log(this.taskList);
+  }
 }
